75eddc1c69f479ec80617ee3083977bb09f7c0b1
Millena Fernandes <millena.fernandes@driven.cx>
2020-09-24 12:45:04 -0300

    Aula06 - State

--- Starter/ReactJS/gostack/src/pages/main/index.js
+++ Starter/ReactJS/gostack/src/pages/main/index.js
@@ -3,6 +3,10 @@ import api from '../../services/api';
 
 export default class Main extends Component {
 
+  state = {
+    products: [],
+  };
+
   componentDidMount() {
     this.loadProducts();
   }
@@ -11,9 +15,16 @@ export default class Main extends Component {
     const response = await api.get('/products');
 
     console.log(response.data.docs);
+    this.setState({products: response.data.docs})
   }
 
   render() {
-    return <h1>Hello</h1>
+    return(
+      <div className="product-list">
+      {this.state.products.map(product => (
+        <h2 key={product._id}>{product.title}</h2>
+      ))}
+      </div>
+    )
   }
 }
