0b88eb994d1e913704d6a4b61441408222763b31
Millena Fernandes <millena.fernandes@driven.cx>
2020-09-25 17:56:42 -0300

    Aula04

--- Starter/Javascript ES6/main.js
+++ Starter/Javascript ES6/main.js
@@ -1,11 +1,38 @@
-alert('Teste');
-
-class Teste {
-  method () {
-
+class List {
+  constructor() {
+    this.data = [];
   }
 
-  outro() {
-    
+  add(data) {
+    this.data.push(data);
+    console.log(this.data);
   }
 }
+
+class TodoList extends List{
+  constructor() {
+    super();
+
+    this.usuario= 'Millena';
+  }
+
+  mostraUsuario() {
+    console.log(this.usuario);
+  }
+}
+
+const MinhaLista = new TodoList();
+
+document.getElementById('novotodo').onclick = function() {
+  MinhaLista.add('Novo todo');
+}
+
+MinhaLista.mostraUsuario();
+
+class Matematica {
+  static soma(a, b) {
+    return a + b;
+  }
+}
+
+console.log(Matematica.soma(1,2));
